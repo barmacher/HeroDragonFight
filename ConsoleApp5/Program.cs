652699459429e35086cb87bc9d9d3ddfb9c9302e
barmacher <eric-gansales98@list.ru>
2022-11-04 13:51:39 +0600

    Сделал дз ООП3

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -2,28 +2,41 @@
  
 try
 {
-    Console.WriteLine("Введите имя");
-    string name = Console.ReadLine();
-    Console.WriteLine("Введите хп");
-    double hp = double.Parse(Console.ReadLine());
-    Hero Vasya = new Hero(name, hp);
-    if (name == "Vasya")
+    Console.WriteLine("Введите имя нового героя");
+    string heroName = Console.ReadLine();
+    Console.WriteLine("Введите хп нового героя");
+    double heroHp = double.Parse(Console.ReadLine());
+    Hero Vasya = new Hero(heroName, heroHp);
+    if (heroName != "Vasya" && heroHp <100)
     {
-        Console.WriteLine($"Имя - {name} и {hp}");
+        throw new Exception("Неверное имя");
+
     }
     else
     {
-        throw new Exception("Неверное имя");
+        Console.WriteLine($"Имя созданного героя - {heroName} и {heroHp}");
     }
-
-//Hero Vasya = new Hero("Vasya", 100);
-Dragon Smaug = new Dragon("Smaug", 500);
-double firstAttack = Vasya.Bite(Smaug);
-Smaug.TakeDamage(firstAttack);
-Console.WriteLine("Smaug breathes fire...");
-double secondAttack = Smaug.Incinirate(Vasya);
-Vasya.TakeDamage(secondAttack);
-Vasya.Heal();
+    Console.WriteLine("Введите имя нового дракона");
+    string dragonName = Console.ReadLine();
+    Console.WriteLine("Введите хп нового дракона");
+    double dragonHp = double.Parse(Console.ReadLine());
+    if(dragonName != "Smaug" && dragonHp < 300)
+    {
+        throw new Exception("Имя дракона должно быть Смауг, и хп должно быть не меньше 300");
+    }
+    else
+    {
+        Console.WriteLine($"Имя созданного героя {dragonName} здоровье {dragonHp}");
+    }
+    //Hero Vasya = new Hero("Vasya", 100);
+    //Dragon Smaug = new Dragon("Smaug", 500);
+    Dragon Smaug = new Dragon(dragonName, dragonHp);
+    double firstAttack = Vasya.Bite(Smaug);
+    Smaug.TakeDamage(firstAttack);
+    Console.WriteLine("Smaug breathes fire...");
+    double secondAttack = Smaug.Incinirate(Vasya);
+    Vasya.TakeDamage(secondAttack);
+    Vasya.Heal();
 }
 catch (Exception ex)
 {
